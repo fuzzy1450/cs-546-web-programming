78f1cce1d0c47be0a2134894071dd68724edab20
Evan Kupsch <ievank@me.com>
2024-09-18 11:07:35 -0400

    arrayAnalysis Partial Implementation

--- Lab2/arrayUtils.js
+++ Lab2/arrayUtils.js
@@ -2,8 +2,52 @@
       using the ES6 syntax. 
       DO NOT CHANGE THE FUNCTION NAMES
 */
+let assertNum = (v, varName) => {
+  if (typeof v !== 'number') throw `${varName || v} must be a number. Recieved type ${typeof v}`;
+  if (isNaN(v)) throw `${varName || v} is NaN`;
 
-let arrayAnalysis = (arr) => {};
+}
+
+let assertNumArray = (v, varName) => {
+  if (!Array.isArray(v)) throw `${varName || v} must be an array.`;
+  try{  // i want to shape the error to be more verbose, so im catching this one even though i was told not to
+    for (i in v) {assertNum(v[i], `${varName || "Array"}[${i}]`)}
+  } catch (e) { 
+    throw new Error(`${varName || v} must contain only numbers.`, {cause: e}) // i am propegating the error here.
+  }                                                                           // please dont take off points
+}
+
+let calculateFrequencies = (arr) => {
+  let fqDict = {}
+  for (i in arr) {
+    if(fqDict[arr[i]]) {
+      fqDict[arr[i]]++
+    } else {
+      fqDict[arr[i]]=1
+    }
+  }
+  return fqDict
+}
+
+let calculateAverage = (arr) => {
+  let n = arr.length
+  let s = 0
+  for (i in arr) {
+    s+= arr[i]
+  }
+  return (s/n)
+}
+
+let arrayAnalysis = (arr) => {
+  // first, make sure the array is all numbers
+  assertNumArray(arr, "Arg1")
+
+  arr.sort();
+  let avg = calculateAverage(arr)
+  let fqDict = calculateFrequencies(arr)
+  // TODO: Continue implementing
+
+};
 
 let mergeKeyValuePairs = (...arrays) => {
   //this function takes in a variable number of arrays that's what the ...arrays signifies.  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
