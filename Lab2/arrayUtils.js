882ffbf8e9646e7b37b3c00d59f828b268f066d5
Evan Kupsch <ievank@me.com>
2024-09-23 19:45:01 -0400

    Data Validation Moved

--- Lab2/arrayUtils.js
+++ Lab2/arrayUtils.js
@@ -1,65 +1,4 @@
-/* Todo: Implement the functions below and then export them
-      using the ES6 syntax. 
-      DO NOT CHANGE THE FUNCTION NAMES
-*/
-
-// ----------Type Enforcement Functions----------
-let assertNum = (v, varName) => {
-  if (typeof v !== 'number') throw `${varName || v} must be a number. Recieved type ${typeof v}`;
-  if (isNaN(v)) throw `${varName || v} is NaN`;
-
-}
-
-let assertNumArray = (v, varName) => {
-  if (!Array.isArray(v)) throw `${varName || v} must be an array.`;
-  if (v.length == 0) throw `${varName || v} must have at least one element.`;
-
-  try{  // i want to shape the error to be more verbose, so im catching this one even though i was told not to
-    for (let i in v) {
-      assertNum(v[i], `${varName || "Array"}[${i}]`)
-    }
-  } catch (e) { 
-    throw new Error(`${varName || v} must contain only numbers.`, {cause: e}) // i am propegating the error here.
-  }                                                                           // please dont take off points
-}
-
-let assertEither = (v, varName, a1, a2, a1Name, a2Name) => {  // a special function that throws an error if v does not pass either assertions of a1 or a2.
-  try {                                                       // if v passes either a1 or a2, no error will be thrown
-    a1(v, varName)
-  } catch (e1) {
-    try {
-    a2(v, varName)
-    } catch (e2) {
-      throw new Error(`${varName || v} failed both asserions (${a1Name || a1}, ${a2Name || a2})`, {cause: [e1, e2]})
-    }
-  }
-}
-
-let assertStr = (v, varName) => {
-  if (typeof v !== 'string') throw `${varName || v} must be a string. Recieved type ${typeof v}`
-  if (v==0) throw `${varName || v} must not be empty.` // this catches many things. most of them cant be used as keys for dicts.
-}
-
-let assertArray = (v, varName) => {
-  if (!Array.isArray(v)) throw `(${varName || v}) must be an array.`;
-}
-
-
-let assertKeyPair = (v, varName) =>{
-  assertArray(v, varName)
-  if (v.length != 2) throw `(${varName || v}) must have two elements`;
-  assertEither(v[0], `(${varName || v})[0]`, assertStr, assertNum, "assertStr", "assertNum")
-  assertEither(v[1], `(${varName || v})[1]`, assertStr, assertNum, "assertStr", "assertNum")
-}
-
-let assertPairArray = (v, varName) => {
-  assertArray(v, varName)
-  if (v.length == 0) throw `${varName || v} must have at least one element`;
-  
-  for(let i in v){
-    assertKeyPair(v[i], `Arg${i}`)
-  }
-}
+import * as paramUtils from './paramUtils.js'
 
 // ---------Helper Functions---------
 // These are helper functions. They will not be exported, and therefore will assume that parameters are valid.
@@ -203,7 +142,7 @@ let objEqualityHelper = (obj1, obj2) => {
 
 export let arrayAnalysis = (arr) => {
   // first, make sure the array is all numbers
-  assertNumArray(arr, "Arg1")
+  paramUtils.assertNumArray(arr, "Arg1")
 
   arr.sort( (a,b) => a-b );
 
@@ -236,7 +175,7 @@ export let arrayAnalysis = (arr) => {
 
 
 export let mergeKeyValuePairs = (...arrays) => {
-  assertPairArray(arrays,"Arguments")
+  paramUtils.assertPairArray(arrays,"Arguments")
 
 
   let KVPairs = {}
@@ -276,7 +215,7 @@ export let deepArrayEquality = (...arrays) => {
   let result = true // we dont want to short circuit the moment we find an inequality, so we have to remember the inequality until the entirety of each array has been checked. 
                     // otherwise, `dAE([0, Symbol("illegal")], [1, Symbol("Illegal")])` will return false, rather than error
   for (let i in arrays) {
-    assertArray(arrays[i], `Argument #${i}`)
+    paramUtils.assertArray(arrays[i], `Argument #${i}`)
     if (arrays[i].length == 0) throw `Argument #${i} is empty. All arguments must contain at least one element`
   }
 
---
+++ Lab2/paramUtils.js
@@ -0,0 +1,66 @@
+
+// ----------Type Enforcement Functions----------
+export let assertNum = (v, varName) => {
+    if (typeof v !== 'number') throw `${varName || v} must be a number. Recieved type ${typeof v}`;
+    if (isNaN(v)) throw `${varName || v} is NaN`;
+  
+}
+
+export let assertNumArray = (v, varName) => {
+    if (!Array.isArray(v)) throw `${varName || v} must be an array.`;
+    if (v.length == 0) throw `${varName || v} must have at least one element.`;
+  
+    try{  // i want to shape the error to be more verbose, so im catching this one even though i was told not to
+      for (let i in v) {
+        assertNum(v[i], `${varName || "Array"}[${i}]`)
+      }
+    } catch (e) { 
+      throw new Error(`${varName || v} must contain only numbers.`, {cause: e}) // i am propegating the error here.
+    }                                                                           // please dont take off points
+}
+  
+export let assertEither = (v, varName, a1, a2, a1Name, a2Name) => {  // a special function that throws an error if v does not pass either assertions of a1 or a2.
+    try {                                                       // if v passes either a1 or a2, no error will be thrown
+        a1(v, varName)
+    } catch (e1) {
+        try {
+        a2(v, varName)
+        } catch (e2) {
+        throw new Error(`${varName || v} failed both asserions (${a1Name || a1}, ${a2Name || a2})`, {cause: [e1, e2]})
+        }
+    }
+}
+  
+export let assertStr = (v, varName) => { // this also does not allow empty strings
+    if (typeof v !== 'string') throw `${varName || v} must be a string. Recieved type ${typeof v}`
+    if (v==0) throw `${varName || v} must not be empty.` // this catches many things. most of them cant be used as keys for dicts.
+}
+  
+export let assertArray = (v, varName) => {
+    if (!Array.isArray(v)) throw `(${varName || v}) must be an array.`;
+}
+
+export let assertValidIndexArray = (v, varName, max, min=0) => {
+    assertNumArray(v, varName)
+    for (let i in v) {
+        if (v[i] < min) throw `${varName || v}[${i}] must be at least ${min}`;
+        if (v[i] > max) throw `${varName || v}[${i}] cannot be greater than ${max}`;
+    }
+}
+  
+export let assertKeyPair = (v, varName) =>{
+    assertArray(v, varName)
+    if (v.length != 2) throw `(${varName || v}) must have two elements`;
+    assertEither(v[0], `(${varName || v})[0]`, assertStr, assertNum, "assertStr", "assertNum")
+    assertEither(v[1], `(${varName || v})[1]`, assertStr, assertNum, "assertStr", "assertNum")
+}
+  
+export let assertPairArray = (v, varName) => {
+    assertArray(v, varName)
+    if (v.length == 0) throw `${varName || v} must have at least one element`;
+    
+    for(let i in v){
+      assertKeyPair(v[i], `Arg${i}`)
+    }
+}
+  
