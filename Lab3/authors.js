9ebf68e995f169e49701e130df39d1538c0cdc80
Evan Kupsch <ievank@me.com>
2024-09-30 19:41:35 -0400

    GetAuthorById implementation

--- Lab3/authors.js
+++ Lab3/authors.js
@@ -1,16 +1,14 @@
 import axios from 'axios';
 import * as paramUtils from './paramUtils.js'
 
-//TODO EXPORT AND IMPLEMENT THE FOLLOWING FUNCTIONS IN ES6 FORMAT
-//Authors data link: https://gist.githubusercontent.com/graffixnyc/a086a55e04f25e538b5d52a095fe4467/raw/e9f835e9a5439a647a24fa272fcb8f5a2b94dece/authors.json
 
-//you must use axios to get the data
+let lastNameCompare = (a, b) => a.last_name.localeCompare(b.last_name);
 
-class AuthorData {
+class AuthorData { // an object we will use to cache the data
     static d = null;
     static async load() {
             const { data } = await axios.get('https://gist.githubusercontent.com/graffixnyc/a086a55e04f25e538b5d52a095fe4467/raw/e9f835e9a5439a647a24fa272fcb8f5a2b94dece/authors.json')
-            this.d = data // this will be the array of author objects
+            this.d = data.sort(lastNameCompare)
     };
 
     static async get() {
@@ -18,14 +16,33 @@ class AuthorData {
         return this.d;
     };
 
+    static async firstMatch(field, value) {
+        let arr = await this.get()
+        for (let i in arr) {
+            let author = arr[i];
+            if (author[field] == value) return author;
+        }
+
+        throw 'no match';
+    };
+
 }
 
-const getAuthorById = async (id) => {};
+export const getAuthorById = async (id) => {
+    paramUtils.assertStr(id, "Author ID");
+    let trimId = id.trim();
+    paramUtils.assertStr(id, "Author ID (trimmed)");
+    try{
+        return AuthorData.firstMatch("id", id)
+    } catch (e) {
+        throw 'author not found'
+    }
+};
 
-const authorsMultipleGenres = async () => {};
+export const authorsMultipleGenres = async () => {};
 
-const averagePageCount = async (firstName, lastName) => {};
+export const averagePageCount = async (firstName, lastName) => {};
 
-const getAuthorsByAgeRange = async (minAge, maxAge) => {};
+export const getAuthorsByAgeRange = async (minAge, maxAge) => {};
 
-const authorsByGenre = async (genre) => {};
+export const authorsByGenre = async (genre) => {};
