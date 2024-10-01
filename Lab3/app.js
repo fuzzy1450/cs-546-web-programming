7a377e6312bb826a656c7eed84bfa9d9415d1e51
Evan Kupsch <ievank@me.com>
2024-09-30 20:26:16 -0400

    Various Tests

--- Lab3/app.js
+++ Lab3/app.js
@@ -10,13 +10,31 @@ Note:
 2. Do not create any other files beside the 'package.json' - meaning your zip should only have the files and folder in this stub and a 'package.json' file.
 3. Submit all files (including package.json) in a zip with your name in the following format: LastName_FirstName.zip.
 4. DO NOT submit a zip containing your node_modules folder.
-
-import * as authors from "./authors.js");
-
-    try{
-        const authorData = await authors.getAuthorById("123");
-        console.log (authorData);
-    }catch(e){
-        console.log (e);
-    }
 */
+
+import * as authors from "./authors.js";
+import * as books from "./books.js";
+
+
+
+try{
+    const authorData = await authors.getAuthorById("1871e6d7-551f-41cb-9a07-08240b86c95c");
+    console.log (authorData);
+}catch(e){
+    console.log (e);
+}
+
+try{
+    const authorList = await authors.authorsMultipleGenres();
+    console.log (authorList);
+}catch(e){
+    console.log (e);
+}
+
+
+try{
+    const bookData = await books.getBookById("4efdb199-5a0f-4410-bded-ce07990c6aa4");
+    console.log (bookData);
+}catch(e){
+    console.log (e);
+}
