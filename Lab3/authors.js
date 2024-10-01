afc72fdf1158a2c9fda236c7ede12e1f0ae45d9a
Evan Kupsch <ievank@me.com>
2024-10-01 19:19:35 -0400

    AuthorData Objectized

--- Lab3/authors.js
+++ Lab3/authors.js
@@ -6,19 +6,39 @@ import * as books from "./books.js";
 let lastNameCompare = (a, b) => a.last_name.localeCompare(b.last_name);
 
 class AuthorData { // an object we will use to cache the data
-    static d = null;
+    static loaded = null;
     static async load() {
             const { data } = await axios.get('https://gist.githubusercontent.com/graffixnyc/a086a55e04f25e538b5d52a095fe4467/raw/e9f835e9a5439a647a24fa272fcb8f5a2b94dece/authors.json')
-            this.d = data
+            this.loaded = data
     };
-
+    
     static async get() {
-        if (!this.d) await this.load();
-        return this.d;
+        if (!this.loaded) await this.load();
+        return new AuthorData(this.loaded);
     };
 
-    static async firstMatch(field, value) {
-        let arr = await this.get()
+    constructor (d){
+        this.data = d;
+    }
+
+    toArray() {
+        return this.data
+    }
+
+    filter(field, value, discardCase=false) {
+        let arr = this.data
+        let ret = []
+        for (let i in arr) {
+            let author = arr[i];
+            if (!discardCase && author[field] == value) ret.push(author);
+            if (discardCase && author[field].toLowerCase() == value.toLowerCase()) ret.push(author);
+        }
+
+        return new AuthorData(ret);
+    }
+
+    firstMatch(field, value) {
+        let arr = this.data
         for (let i in arr) {
             let author = arr[i];
             if (author[field] == value) return author;
@@ -31,7 +51,7 @@ class AuthorData { // an object we will use to cache the data
 
 export const utils = { // etc. utilities
     async namesToAuthor(first, last) { // retrieve an author by first and last name. case insensitive
-        let authors = await AuthorData.get();
+        let authors = (await AuthorData.get()).toArray();
         for (let i in authors) {
             let author = authors[i];
             if (author.first_name.toLowerCase() == first.toLowerCase() 
@@ -49,7 +69,8 @@ export const getAuthorById = async (id) => {
     let trimId = id.trim();
     paramUtils.assertStr(id, "Author ID (trimmed)");
     try{
-        return AuthorData.firstMatch("id", id)
+        let authors = await AuthorData.get()
+        return authors.firstMatch("id", id)
     } catch (e) {
         throw 'author not found'
     }
@@ -57,7 +78,7 @@ export const getAuthorById = async (id) => {
 
 export const authorsMultipleGenres = async () => {
     let r = []
-    let authors = (await AuthorData.get()).sort(lastNameCompare);
+    let authors = (await AuthorData.get()).toArray().sort(lastNameCompare);
     for (let i in authors) {
         let author = authors[i];
         
