4f83a5db6ef7b0fda981d369b7153d285dbb32ea
Evan Kupsch <ievank@me.com>
2024-10-01 18:36:40 -0400

    doesnt need to be sorted

--- Lab3/authors.js
+++ Lab3/authors.js
@@ -9,7 +9,7 @@ class AuthorData { // an object we will use to cache the data
     static d = null;
     static async load() {
             const { data } = await axios.get('https://gist.githubusercontent.com/graffixnyc/a086a55e04f25e538b5d52a095fe4467/raw/e9f835e9a5439a647a24fa272fcb8f5a2b94dece/authors.json')
-            this.d = data.sort(lastNameCompare)
+            this.d = data
     };
 
     static async get() {
