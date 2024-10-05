0cc027790d22d2756a37aa202faa056103384b3e
Evan Kupsch <ievank@me.com>
2024-10-04 22:44:24 -0400

    Lab4 Stub

---
+++ Lab4/app.js
@@ -0,0 +1,19 @@
+/*
+
+Create a team of your choice.
+Log the newly created team. (Just that team, not all teams)
+Create another team of your choice.
+Query all team, and log them all
+Create the 3rd team of your choice.
+Log the newly created 3rd team. (Just that team, not all team)
+Move the first team
+Log the first team with the updated info. 
+Remove the second team you created.
+Query all teams, and log them all
+Try to create a team with bad input parameters to make sure it throws errors.
+Try to remove a team that does not exist to make sure it throws errors.
+Try to rename a team that does not exist to make sure it throws errors.
+Try to rename a team passing in invalid data to make sure it throws errors.
+Try getting a team by ID that does not exist to make sure it throws errors.
+
+*/
---
+++ Lab4/data/teams.js
@@ -0,0 +1,19 @@
+// TODO: Export and implement the following functions in ES6 format
+const createTeam = async (
+  name,
+  sport,
+  yearFounded,
+  city,
+  state,
+  stadium,
+  championshipsWon,
+  players
+) => {};
+
+const getAllTeams = async () => {};
+
+const getTeamById = async (id) => {};
+
+const removeTeam = async (id) => {};
+
+const moveTeam = async (id, newCity, newState, newStadium) => {};
