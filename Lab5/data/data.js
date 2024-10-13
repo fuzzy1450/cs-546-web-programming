231cf24fd27cbe34a3730cfdace38c4010322a30
Evan Kupsch <ievank@me.com>
2024-10-12 22:04:45 -0400

    Lab5 Stub

---
+++ Lab5/data/data.js
@@ -0,0 +1,11 @@
+/*Here, you can export the data functions
+to get the authors, books, getAuthorByID, getBookById.  You will import these functions into your routing files and call the relevant function depending on the route. 
+*/
+
+const getAuthors = async () => {};
+
+const getBooks = async () => {};
+
+const getAuthorById = async (id) => {};
+
+const getBookById = async (id) => {};
