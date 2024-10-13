7ab648cc63ec48c55299c5723824a328827e7499
Evan Kupsch <ievank@me.com>
2024-10-12 22:14:10 -0400

    Imported Authors and Books from Lab3

---
+++ Lab5/data/authors.js
@@ -0,0 +1,211 @@
+import axios from 'axios';
+import * as paramUtils from '../paramUtils.js';
+import * as books from "./books.js";
+
+
+let lastNameCompare = (a, b) => a.last_name.localeCompare(b.last_name);
+
+class AuthorData { // an object we will use to cache the data
+    static loaded = null;
+    static async load() {
+            const { data } = await axios.get('https://gist.githubusercontent.com/graffixnyc/a086a55e04f25e538b5d52a095fe4467/raw/e9f835e9a5439a647a24fa272fcb8f5a2b94dece/authors.json')
+            this.loaded = data
+    };
+    
+    static async get() {
+        if (!this.loaded) await this.load();
+        return new AuthorData(this.loaded);
+    };
+
+    constructor (d){
+        this.data = d;
+    }
+
+    toArray() {
+        return this.data
+    }
+
+    filterEQ(field, value, discardCase=false) {     // filter with an equals comparison
+        let arr = this.data
+        let ret = []
+        for (let i in arr) {
+            let author = arr[i];
+            if (!discardCase && author[field] == value) ret.push(author);
+            if (discardCase && author[field].toLowerCase() == value.toLowerCase()) ret.push(author);
+        }
+
+        return new AuthorData(ret);
+    }
+
+
+    filterAgeMin(min) {    // filter the authors by age maximum (inclusive)
+        let arr = this.data
+        let ret = []
+        for (let i in arr) {
+            let author = arr[i];
+            let age = utils.authorToAge(author);
+            if (age >= min) ret.push(author);
+        }
+
+        return new AuthorData(ret);
+    }
+
+
+    filterAgeMax(max) {    // filter the authors by age maximum (inclusive)
+        let arr = this.data
+        let ret = []
+        for (let i in arr) {
+            let author = arr[i];
+            let age = utils.authorToAge(author);
+            if (age <= max) ret.push(author);
+        }
+
+        return new AuthorData(ret);
+    }
+
+    async filterGenre(genre) {    // not case sensitive
+        let arr = this.data;
+        let ret = [];
+        for (let i in arr){
+            let author = arr[i]
+            let bookList = await books.utils.IDsToBooks(author.books);
+            let genreList = books.utils.booksToGenres(bookList).map((x)=>x.toLowerCase());
+            if (genreList.includes(genre.toLowerCase())) ret.push(author);
+        }
+
+        return new AuthorData(ret);
+    }
+
+
+
+    firstMatch(field, value) {
+        let arr = this.data
+        for (let i in arr) {
+            let author = arr[i];
+            if (author[field] == value) return author;
+        }
+
+        throw 'no match';
+    };
+
+    first() {
+        return this.data[0];
+    }
+
+}
+
+export const utils = { // etc. utilities
+    async namesToAuthor(first, last) { // retrieve an author by first and last name. case insensitive
+        let authors = await AuthorData.get();
+        let author = authors.filterEQ("first_name", first, true).filterEQ("last_name", last, true).first();
+
+        return author;
+    },
+
+    authorToAge(author) {   // this function calculates two ages via two different methods to try and obtain concensus
+        // the first method, checkes the duration of life. (not always equal to age)
+        let now = new Date();
+        let then = new Date(author.date_of_birth);
+        let robotAge = now-then;
+        let roboYears = robotAge/(1000*60*60*24*365);
+
+        // now we check the birthday by counting birthdays
+        let birthday = author.date_of_birth.split("/");
+        let year = now.getFullYear();
+        let month = now.getMonth() + 1;
+        let day = now.getDay();
+
+        let age = (year - parseInt(birthday[2])); // rough birthday estimate. assumes they already had a birthday this year
+        age--; // invert the assumption - they did not have a birthday this year.
+        // now we need to check if they had a birthday this year. if they did, we need to add 1.
+        if(month > parseInt(birthday[0])) {
+            age++;
+        } else if (month == parseInt(birthday[0]) && day >= parseInt(birthday[1])) {
+            age++;
+        }
+        //if(parseInt(roboYears) != age) console.debug(`Age disparity. r: ${roboYears} c: ${age} b: ${author.date_of_birth}`);
+
+        return age; // counting years is more accurate to find their age, rather than countingthe duration since their birth.
+        // but if for some reason you guys expect me to use Date() - Date(), then just swap to the below return statement
+        //return parseInt(roboYears);
+    }
+}
+
+
+export const getAuthorById = async (id) => {
+    paramUtils.assertStr(id, "Author ID");
+    let trimId = id.trim();
+    paramUtils.assertStr(trimId, "Author ID (trimmed)");
+    try{
+        let authors = await AuthorData.get()
+        return authors.firstMatch("id", trimId)
+    } catch (e) {
+        throw 'author not found'
+    }
+};
+
+export const authorsMultipleGenres = async () => {
+    let r = []
+    let authors = (await AuthorData.get()).toArray().sort(lastNameCompare);
+    for (let i in authors) {
+        let author = authors[i];
+        
+        let bookList = await books.utils.IDsToBooks(author.books)
+
+        
+        let authorGenres = books.utils.booksToGenres(bookList)
+        if (authorGenres.length > 1) r.push(`${author.first_name} ${author.last_name}`);
+    }
+    return r;
+};
+
+export const averagePageCount = async (firstName, lastName) => {
+    paramUtils.assertStr(firstName, "First Name");
+    paramUtils.assertStr(lastName, "Last Name");
+    let trimFirst = firstName.trim();
+    let trimLast = lastName.trim()
+    paramUtils.assertStr(trimFirst, "First Name (trimmed)");
+    paramUtils.assertStr(trimLast, "Last Name (trimmed)");
+
+    let author = await utils.namesToAuthor(trimFirst, trimLast);
+
+    if (!author) throw 'Author Not Found';
+    
+    let bookList = await books.utils.IDsToBooks(author.books);
+    let sum = 0;
+    let n = 0;
+    for (let i in bookList) {
+        sum+= bookList[i].pageCount;
+        n++;
+    }
+    return (n ? sum/n : 0); // if there are no books (n=0) then we cant divide sum by n.
+
+};
+
+export const getAuthorsByAgeRange = async (minAge, maxAge) => {
+    paramUtils.assertWholeNumber(minAge, "1st Argument");
+    paramUtils.assertWholeNumber(maxAge, "2nd Argument");
+    if (minAge < 1) throw `1st Argument must be greater than or equal to 1`;
+    if (minAge >= maxAge) throw `minAge cannot be greater or equal to maxAge`;
+
+    let Authors = (await AuthorData.get()).filterAgeMin(minAge).filterAgeMax(maxAge);
+
+    let ret = Authors.toArray();
+    if (ret.length == 0) throw `No Authors found between the ages of ${minAge} and ${maxAge}`;
+
+    return ret;
+};
+
+export const authorsByGenre = async (genre) => {
+    paramUtils.assertStr(genre, "1st Argument")
+    let genreTrim = genre.trim();
+    paramUtils.assertStr(genreTrim, "1st Argument (trimmed)");
+
+    let Authors = (await (await AuthorData.get()).filterGenre(genreTrim)).toArray();
+    
+    if (Authors.length == 0) throw 'Genre does not exist in dataset';
+
+    return Authors;
+
+};
+
---
+++ Lab5/data/books.js
@@ -0,0 +1,190 @@
+import axios from 'axios';
+import * as paramUtils from '../paramUtils.js';
+
+class BookData { // an object we will use to cache the data
+    static loaded = null;
+    static async load() {
+            const { data } = await axios.get('https://gist.githubusercontent.com/graffixnyc/3381b3ba73c249bfcab1e44d836acb48/raw/e14678cd750a4c4a93614a33a840607dd83fdacc/books.json')
+            this.loaded = data // this will be the array of book objects
+    };
+
+    static async get() {
+        if (!this.loaded) await this.load();
+        return new BookData(this.loaded);
+    };
+
+    constructor (d){
+        this.data = d;
+    }
+
+    toArray() {
+        return this.data
+    }
+
+    first() {
+        return this.data[0];
+    }
+
+    firstMatch(field, value) {
+        let arr = this.data
+        for (let i in arr) {
+            let book = arr[i];
+            if (book[field] == value) return book;
+        }
+
+        throw 'no match';
+    };
+
+    filterGenre(genre) {    // not case sensitive
+        let arr = this.data;
+        let ret = [];
+        for (let i in arr){
+            let book = arr[i];
+            let genreList = book.genres.map((x)=>x.toLowerCase());
+            if (genreList.includes(genre.toLowerCase())) ret.push(book);
+        }
+
+        return new BookData(ret);
+    }
+
+    calculateAveragePrice(){
+        let arr = this.data;
+        let sum = 0;
+        let n = 0;
+
+        for (let i in arr) {
+            let book = arr[i];
+            sum+=book.price;
+            n++;
+        }
+
+        return n ? sum/n : 0;
+    }
+
+}
+
+export const utils = { // etc. utilities
+    IDsToBooks: async(ids) => { // turn an array of ids into an array of books
+        let r = [];
+        for(let i in ids) {
+            r.push(await getBookById(ids[i]))
+        }
+        return r;
+    },
+
+    booksToGenres: (books)=>{ // turn an array of books into an array of unique genres
+        let s = {};
+        for (let i in books){
+            let book = books[i]
+            for (let j in book.genres){
+                let genre = book.genres[j]
+                if(!s[genre]) s[genre]=0;
+                s[genre]++;
+            }
+        }
+
+        let r=[];
+        for (let i in s){
+            r.push(i);
+        }
+        return r;
+    }
+}
+
+export const getBookById = async (id) => {
+    paramUtils.assertStr(id, "Book ID");
+    let trimId = id.trim();
+    paramUtils.assertStr(trimId, "Book ID (trimmed)");
+    let books = await BookData.get();
+
+    try{
+        return books.firstMatch("id", trimId)
+    } catch (e) {
+        throw 'book not found'
+    }
+};
+
+export const booksByFormat = async () => {
+    let books = await BookData.get();
+    let bookList = books.toArray();
+    let formatDict = {};
+    for (let i in bookList) {
+        let book = bookList[i];
+        let formatList = book.format;
+        for (let j in formatList) {
+            let fmt = formatList[j]
+            if(formatDict[fmt]) {formatDict[fmt]++}
+            else {formatDict[fmt] = 1};
+        }
+    }
+    return formatDict;
+
+};
+
+export const mostPopularGenre = async () => {
+    let books = await BookData.get();
+    let bookList = books.toArray();
+    let genreDict = {};
+
+    for (let i in bookList) {
+        let book = bookList[i];
+        let genreList = book.genres;
+        for (let j in genreList) {
+            let genre = genreList[j]
+            if(genreDict[genre]) {genreDict[genre]++}
+            else {genreDict[genre] = 1};
+        }
+    }
+
+    let s = []
+    let m = 0;
+
+    for (let genre in genreDict) {
+        if(genreDict[genre] > m) {
+            s = [genre];
+            m = genreDict[genre];
+        } else if (genreDict[genre] == m) {
+            s.push(genre);
+        }
+    }
+    let alphaCmp = (a, b) => a.localeCompare(b);
+
+    s.sort(alphaCmp);
+    if(s.length == 1) return s[0];
+    return s;
+
+};
+
+export const booksByPublisher = async (publisher) => {
+    paramUtils.assertStr(publisher, "1st Argument");
+    let trimPublisher = publisher.trim().toLowerCase();
+    paramUtils.assertStr(trimPublisher, "1st Argument (trimmed)");
+
+    let books = await BookData.get();
+    let bookList = books.toArray();
+    
+    let ret = [];
+
+    for (let i in bookList) {
+        let book = bookList[i];
+        if(book.publisher.toLowerCase().trim() == trimPublisher) ret.push(book);
+    }
+
+    if (ret.length == 0) throw 'Publisher does not exist';
+
+    return ret;
+
+};
+
+export const averagePriceByGenre = async (genre) => {
+    paramUtils.assertStr(genre, "1st Argument");
+    let trimGenre = genre.trim().toLowerCase();
+    paramUtils.assertStr(trimGenre, "1st Argument (trimmed)");
+
+    let books = (await BookData.get()).filterGenre(trimGenre);
+    let avgPrice = books.calculateAveragePrice();
+
+    let rounded = parseInt(avgPrice * 100)/100;
+
+    return rounded;
+};
