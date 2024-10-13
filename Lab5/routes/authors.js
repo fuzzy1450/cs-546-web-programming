d44f90ef2bf9fd20d02f702dece06cfeb8c10993
Evan Kupsch <ievank@me.com>
2024-10-12 23:15:41 -0400

    bugfix

--- Lab5/routes/authors.js
+++ Lab5/routes/authors.js
@@ -1,10 +1,10 @@
 //You will code the route in this file
 //Lecture Code Refernece -> https://github.com/stevens-cs546-cs554/CS-546/tree/master/lecture_05/routes
 
-//You can import your getAuthors() function in the /data/data.js file 3 to return the list of authors and call it in the /authors route.  You can also import your getAuthorById function and call it in the :/id route.
-import * as data from './data/data.js';
+import * as data from '../data/data.js';
+import express from 'express';
 
-const router = express.Router();
+export const router = express.Router();
 
 router.route('/')
     .get( async (req, res) => {
