585e68a746312882b986aa22205698169b48d52b
Evan Kupsch <ievank@me.com>
2024-11-02 01:25:19 -0400

    Data Functions

--- Lab6/data/games.js
+++ Lab6/data/games.js
@@ -1,18 +1,146 @@
 // This data file should export all functions using the ES6 standard as shown in the lecture code
+const {teams} = await import("../config/mongoCollections.js");
+const {ObjectId} = await import('mongodb');
 
-const createGame = async (
+import * as helpers from '../helpers.js';
+
+let assertTeamExists = async (id)=>{
+  helpers.assertStr(id, "1st Argument");
+  let idTrim = id.trim();
+
+  if(!ObjectId.isValid(idTrim)) throw `${id} is not a valid ObjectID`;
+
+  let col = await teams();
+
+  let res = await col.findOne({"_id" : new ObjectId(idTrim)});
+
+  if(!res) throw `Object with id ${idTrim} not found`;
+}
+
+let assertSameSport = async (id1, id2) => {
+  let t1 = id1.trim();
+  let t2 = id2.trim();
+  assertTeamExists(t1)
+  assertTeamExists(t2)
+  
+  let col = await teams();
+
+  let r1 = await col.findOne({"_id" : new ObjectId(t1)});
+  let r2 = await col.findOne({"_id" : new ObjectId(t2)});
+
+  if(r1.sport != r2.sport) throw `The two teams do not play the same sport`
+}
+
+const getTeamByGameId = async (gameId) => {
+  helpers.assertStr(gameId)
+  let gameIdTrim = gameId.trim()
+  if(!ObjectId.isValid(gameIdTrim)) throw `${gameId} is not a valid ObjectID`;
+  let col = await teams();
+  let team = await col.findOne({'games._id': ObjectId(gameIdTrim)});
+  if(!team) throw `No game found with ID ${gameIdTrim}`;
+  return team;
+}
+
+
+export const createGame = async (
   teamId,
   gameDate,
   opposingTeamId,
   homeOrAway,
   finalScore,
   win
-) => {};
+) => {
+  assertTeamExists(teamId);
+  assertTeamExists(opposingTeamId);
+  
+  let newGame = helpers.createValidGame(teamId, gameDate, opposingTeamId, homeOrAway, finalScore, win)
 
-const getAllGames = async (teamId) => {};
+  newGame.opposingTeamId = new ObjectId(opposingTeamId.trim())
 
-const getGame = async (gameId) => {};
+  let teamIdTrim = teamId.trim()
 
-const updateGame = async (gameId, updateObject) => {};
+  
+  newGame["_id"] = new ObjectId();
 
-const removeGame = async (gameId) => {};
+  let col = await teams();
+  let oldTeam = await col.findOne({_id: ObjectId(teamIdTrim)});
+  let WLR = oldTeam.winLossCount.split("-")
+  win ? WLR[0]=`${parseInt(WLR[0])+1}` : WLR[1]=`${parseInt(WLR[1])+1}`
+
+
+
+  await col.updateOne({_id: ObjectId(teamIdTrim)}, {$push: {games: newGame}, $set:{winLossCount: WLR.join('-')}})
+  return await col.findOne({_id: ObjectId(teamIdTrim)});
+};
+
+export const getAllGames = async (teamId) => {
+  assertTeamExists(teamId);
+
+  let col = await teams();
+  let team = await col.findOne({_id: ObjectId(teamId.trim())});
+
+  return team.games;
+};
+
+export const getGame = async (gameId) => {
+  helpers.assertStr(gameId)
+  let gameIdTrim = gameId.trim()
+  if(!ObjectId.isValid(gameIdTrim)) throw `${gameId} is not a valid ObjectID`;
+  let col = await teams();
+  let game = await col.findOne({'games._id': ObjectId(gameIdTrim)}, {projection: {'games.$': 1}});
+  if(!game) throw `No game found with ID ${gameIdTrim}`;
+  return game;
+};
+
+export const updateGame = async (gameId, updateObject) => {
+  helpers.assertStr(gameID);
+  let gameIdTrim = gameId;
+  let oldGame = getGame(gameIdTrim); // this does data validation for us :)
+
+  helpers.assertObject(updateObject, "updateObject")
+  let n = 0
+  for(let i in updateObject) n++;
+  if (n==0) throw `updateObject cannot be empty`;
+  validatePartialGame(updateObject)
+
+  let team = await getTeamByGameId(gameIdTrim);
+
+  if(updateObject.opposingTeamId) {
+    assertTeamExists(updateObject.opposingTeamId, "Opposing Team ID");
+    assertSameSport(team["_id"], updateObject.opposingTeamId)
+  }
+
+  let WLR = team.winLossCount.split("-")
+  if((typeof updateObject.win) == 'boolean') {
+    if(updateObject.win && !oldGame.win) {
+      WLR[0]=`${parseInt(WLR[0])+1}`
+      WLR[1]=`${parseInt(WLR[1])-1}`
+    } 
+    else if (!updateObject.win && oldGame.win) {
+      WLR[0]=`${parseInt(WLR[0])-1}`
+      WLR[1]=`${parseInt(WLR[1])+1}`
+    }
+  } 
+
+  let col = await teams();
+  await col.updateOne({"games._id": ObjectId(gameIdTrim)}, {$set: {'games.$': {...updateObject, _id: ObjectId(gameIdTrim)}, winLossCount: WLR.join('-')}})
+  
+};
+
+export const removeGame = async (gameId) => {
+  helpers.assertStr(gameID);
+  gameIdTrim = gameId.trim();
+
+  let col = await teams();
+  let oldGame = await getGame(gameIdTrim);
+  let team = await getTeamByGameId(gameIdTrim);
+  
+
+  let WLR = team.winLossCount.split("-")
+  oldGame.win ? WLR[0]=`${parseInt(WLR[0])-1}` : WLR[1]=`${parseInt(WLR[1])-1}`
+
+
+  await col.updateOne({"games._id": ObjectId(gameIdTrim)}, {$pull: {'games': {_id: ObjectId(gameIdTrim)}, winLossCount: WLR.join('-')}})
+
+  return await col.findOne({"_id": ObjectId(team._id)})
+};
--- Lab6/data/teams.js
+++ Lab6/data/teams.js
@@ -1,6 +1,9 @@
 // This data file should export all functions using the ES6 standard as shown in the lecture code
+const {teams} = await import("../config/mongoCollections.js");
+const {ObjectId} = await import('mongodb');
+import * as helpers from '../helpers.js';
 
-const createTeam = async (
+export const createTeam = async (
   name,
   sport,
   yearFounded,
@@ -9,15 +12,58 @@ const createTeam = async (
   stadium,
   championshipsWon,
   players
-) => {};
+) => {
 
-const getAllTeams = async () => {};
+  let newTeam = helpers.createValidTeam(name, sport, yearFounded, city, state, stadium, championshipsWon, players)
+  let col = await teams();
 
-const getTeamById = async (id) => {};
+  let res = await col.insertOne(newTeam)
 
-const removeTeam = async (id) => {};
+  if(res.insertedId) {
+    newTeam["_id"] = res.insertedId.toString();
+    return newTeam;
+  } 
+  else { throw `Insertion Failure - Did not get an insertedID. Got ${res}` }
 
-const updateTeam = async (
+};
+
+export const getAllTeams = async () => {
+    let col = await teams();
+    let arr = await (col.find()).toArray().map( (team) => ({_id: team["_id"].toString(), name: team.name}) );
+  
+    return arr;
+};
+
+export const getTeamById = async (id) => {
+  helpers.assertStr(id, "1st Argument");
+  let idTrim = id.trim();
+
+  if(!ObjectId.isValid(idTrim)) throw `${id} is not a valid ObjectID`;
+
+  let col = await teams();
+
+  let res = await col.findOne({"_id" : new ObjectId(idTrim)});
+
+  if(!res) throw `Object with id ${idTrim} not found`;
+
+  res["_id"] = res["_id"].toString();
+
+  return res;
+};
+
+export const removeTeam = async (id) => {
+  team = await getTeamById(id); // this will also do validation for us
+
+  let idTrim = id.trim();
+  let col = await teams();
+  let res = await col.deleteOne({"_id" : new ObjectId(idTrim)});
+  
+  if(res.deletedCount!=1) throw `Team with id ${idTrim} not found`;
+
+  return team;
+};
+
+export const updateTeam = async (
   id,
   name,
   sport,
@@ -27,4 +73,19 @@ const updateTeam = async (
   stadium,
   championshipsWon,
   players
-) => {};
+) => {
+  let newTeam = helpers.createValidTeam(name, sport, yearFounded, city, state, stadium, championshipsWon, players)
+  let oldTeam = await getTeamById(id); // this will also do validation for us
+
+  let idTrim = id.trim();
+  let col = await teams();
+
+  let res = await col.updateOne({"_id" : new ObjectId(idTrim)}, newTeam);
+
+  if(res.matchedCount == 0) throw `Team with id ${idTrim} not found`;
+  if(res.modifiedCount == 0) throw `Team with id ${idTrim} was found, but not updated`;
+
+  newTeam["_id"] = idTrim;
+
+  return newTeam;
+};
